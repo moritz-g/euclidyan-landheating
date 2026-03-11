c7d846ca91d9bdb3f56d71c81c56513d2770dc97
Moritz Guenther <mo.guenther@web.de>
2026-03-11 19:03:43 +0100

    Put intake_demo into functions to allow import to other files

--- common/src/intake_demo.py
+++ common/src/intake_demo.py
@@ -26,26 +26,34 @@ combine_kwargs = {
     "join": "outer",
 }
 
+
 # %% load catalog
-force_reload = False
-if os.path.exists("cat_piControl.json") and not force_reload:
-    cat = intake.open_esm_datastore("cat_piControl.json")
-else:
-    col_url = "/work/ik1017/Catalogs/dkrz_cmip6_disk.json"
-    col = intake.open_esm_datastore(col_url)
-    query = dict(
-        experiment_id="piControl",
-        table_id=["Amon", "fx"],
-        variable_id=["tas", "sftlf", "areacella"],
-        grid_label=["gn", "gr"],
-        member_id="r1i1p1f1",
-    )
-    cat = col.search(**query)
-    print(cat.df["source_id"].unique())
-    cat.serialize(name="cat_piControl", catalog_type="file")
+def load_cmip6_datasets(query, force_reload=False, catalog_name="cat_piControl"):
+    """
+    Load CMIP6 datasets from catalog based on query parameters.
+
+    Args:
+        query: dict with catalog search parameters (experiment_id, table_id, etc.)
+        force_reload: bool, if True reload catalog from source
+        catalog_name: str, name of local catalog file
+
+    Returns:
+        dset_dict: dict of loaded xarray datasets
+    """
+    # Load or create catalog
+    if os.path.exists(f"{catalog_name}.json") and not force_reload:
+        cat = intake.open_esm_datastore(f"{catalog_name}.json")
+    else:
+        col_url = "/work/ik1017/Catalogs/dkrz_cmip6_disk.json"
+        col = intake.open_esm_datastore(col_url)
+        cat = col.search(**query)
+        print(cat.df["source_id"].unique())
+        cat.serialize(name=catalog_name, catalog_type="file")
+
+    # Load datasets
+    return load_from_cat(cat, cdf_kwargs, combine_kwargs)
 
 
-# %% load datasets
 def _sort_uri_by_timerange(uri: str):
     m = re.search(r"_(\d{6})-(\d{6})\.nc$", uri)
     return m.group(1) if m else uri
@@ -55,7 +63,6 @@ def load_key_fallback(cat, key, cdf_kwargs):
     uris = cat[key].df["uri"].drop_duplicates().tolist()
     uris = sorted(uris, key=_sort_uri_by_timerange)
 
-    # explicit concat when combine_by_coords cannot infer dimension coords
     ds = xr.open_mfdataset(
         uris,
         combine="nested",
@@ -108,37 +115,46 @@ def load_from_cat(cat, cdf_kwargs, combine_kwargs):
     return out
 
 
-dset_dict = load_from_cat(cat, cdf_kwargs, combine_kwargs)
-
 # %% list of all models
-models = set()
-for key in dset_dict.keys():
-    model = key.split(".")[1]
-    models.add(model)
-print(models)
+if __name__ == "__main__":
+    # this is how to use the wrapper function
+    query = dict(
+        experiment_id="piControl",
+        table_id=["Amon", "fx"],
+        variable_id=["tas", "sftlf", "areacella"],
+        grid_label=["gn", "gr"],
+        member_id="r1i1p1f1",
+    )
+    dset_dict = load_cmip6_datasets(query)
 
-# %% access a dataset
-model = "MPI-ESM1-2-LR"
-da = (
-    tools.annual_mean_from_monthly(dset_dict[f"CMIP.{model}.piControl.Amon.gn"])
-    .mean("year")
-    .tas.squeeze()
-    .compute()
-)
-da_area = dset_dict[f"CMIP.{model}.piControl.fx.gn"].areacella.squeeze().compute()
-lsm = dset_dict[f"CMIP.{model}.piControl.fx.gn"].sftlf.squeeze().compute()
-fig, ax = plt.subplots(
-    subplot_kw={
-        "projection": ccrs.Robinson(central_longitude=230),
-        "transform": ccrs.PlateCarree(),
-        "frameon": True,
-    }
-)
-da.plot.contourf(ax=ax, transform=ccrs.PlateCarree(), cmap="viridis")
-ax.coastlines()
-tools.plot_sep_outlines(ax)
-ax.set_title(None)
-plt.show()
-# Mean temperature in SEP
-tas_sep = tools.select_sep(da, lsm)
-print(f"Mean temperature in SEP: {tools.fldmean(tas_sep, da_area):.2f} K")
+    models = set()
+    for key in dset_dict.keys():
+        model = key.split(".")[1]
+        models.add(model)
+    print(models)
+
+    # access a dataset
+    model = "MPI-ESM1-2-LR"
+    da = (
+        tools.annual_mean_from_monthly(dset_dict[f"CMIP.{model}.piControl.Amon.gn"])
+        .mean("year")
+        .tas.squeeze()
+        .compute()
+    )
+    da_area = dset_dict[f"CMIP.{model}.piControl.fx.gn"].areacella.squeeze().compute()
+    lsm = dset_dict[f"CMIP.{model}.piControl.fx.gn"].sftlf.squeeze().compute()
+    fig, ax = plt.subplots(
+        subplot_kw={
+            "projection": ccrs.Robinson(central_longitude=230),
+            "transform": ccrs.PlateCarree(),
+            "frameon": True,
+        }
+    )
+    da.plot.contourf(ax=ax, transform=ccrs.PlateCarree(), cmap="viridis")
+    ax.coastlines()
+    tools.plot_sep_outlines(ax)
+    ax.set_title(None)
+    plt.show()
+    # Mean temperature in SEP
+    tas_sep = tools.select_sep(da, lsm)
+    print(f"Mean temperature in SEP: {tools.fldmean(tas_sep, da_area):.2f} K")
